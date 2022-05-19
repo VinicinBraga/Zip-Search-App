90ea8539e36ff9f37addb6781564d97feffa4abe
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-19 17:21:30 -0300

    base react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <h1 className="container">Zip Code Search</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Type your zip code..." />
+        <button className="buttonSearch">Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
