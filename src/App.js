fe3fdaaae7b20e369b0ce60881bd6d67ff987313
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-19 17:40:01 -0300

    Flip title animation css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { FiSearch } from "react-icons/fi";
+import "./App.css";
+
 function App() {
   return (
     <div className="container">
-      <h1 className="container">Zip Code Search</h1>
+      <h1 className="title">Zip Code Search</h1>
       <div className="containerInput">
         <input type="text" placeholder="Type your zip code..." />
-        <button className="buttonSearch">Search</button>
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#fff" />
+        </button>
       </div>
+
+      <main className="main">
+        <h2>Cep: 94949498</h2>
+
+        <span>Rua: teste </span>
+        <span>Complemento: teste </span>
+        <span>Bairro: teste </span>
+        <span>Cidade: teste </span>
+      </main>
     </div>
   );
 }
