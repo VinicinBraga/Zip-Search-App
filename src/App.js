84231f28616c8a99f926497f81a216ca4426600e
VinicinBraga <vinicius@bragamatta.com.br>
2022-05-20 14:26:54 -0300

    App done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import React, { useState } from "react";
+import api from "./services/api";
 import { FiSearch } from "react-icons/fi";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  const handleSearch = async () => {
+    if (input === "") {
+      alert("Enter a zip code in the search field");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert(`Zip code not found`);
+      setInput("");
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="title">Zip Code Search</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Type your zip code..." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Type your zip code..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
-
-      <main className="main">
-        <h2>Cep: 94949498</h2>
-
-        <span>Rua: teste </span>
-        <span>Complemento: teste </span>
-        <span>Bairro: teste </span>
-        <span>Cidade: teste </span>
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>Cep:{cep.cep}</h2>
+          <span>
+            {cep.logradouro} - n°: {cep.complemento}
+          </span>
+          <span>Bairro: {cep.bairro} </span>
+          <span>
+            Cidade: {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
